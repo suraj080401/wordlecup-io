a463066555af759aed2ee33bfe5ac69f5f0fd8e6
Suraj Singh <suraj.singh080401@gmail.com>
2024-02-15 11:39:23 +0530

    feat: adding socket service in server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Server Started");
+import http from "http";
+import SocketService from "./services/socket";
+
+async function init() {
+	const socketService = new SocketService();
+	const httpServer = http.createServer();
+	const PORT = process.env.PORT ? process.env.PORT : 8000;
+
+	socketService.io.attach(httpServer);
+
+	httpServer.listen(PORT, () => {
+		console.log(`Server is running on ${PORT}`);
+	});
+}
+
+init();
---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,15 @@
+import { Server } from "socket.io";
+
+class SocketService {
+	private _io: Server;
+	constructor() {
+		console.log("Init Socket Server");
+		this._io = new Server();
+	}
+
+	get io() {
+		return this._io;
+	}
+}
+
+export default SocketService;
