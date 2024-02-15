aaa5e4e0bd49eaa6a312dd171f83f2895d1e4efc
Suraj Singh <suraj.singh080401@gmail.com>
2024-02-15 15:30:25 +0530

    feat: logic of adding text messages from userinputs

--- apps/web/app/components/ChatContainer.tsx
+++ apps/web/app/components/ChatContainer.tsx
@@ -1,17 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../page.module.css";
 import { IoSend } from "react-icons/io5";
+import SingleChatBox from "./SingleChatBox";
+
+interface IMessage {
+	name: string;
+	message: string;
+	time: string;
+}
 
 export const ChatContainer: React.FC = () => {
+	const [allMessages, setAllMessages] = useState<IMessage[]>([]);
+	const [currentMessage, setCurrentMessage] = useState<string>("");
+
+	const onSendingMessage = () => {
+		const date = new Date();
+		let hours = date.getHours() % 12 || 12;
+		const minutes = date.getMinutes().toString().padStart(2, "0");
+		let stringhours = hours < 10 ? "0" + hours : hours;
+		const currentTimeString: string = `${stringhours}:${minutes}`;
+
+		const newMesage: IMessage = {
+			name: "Suraj Singh",
+			message: currentMessage,
+			time: currentTimeString,
+		};
+
+		setAllMessages((prevArray) => [...prevArray, newMesage]);
+
+		setCurrentMessage("");
+	};
+
 	return (
 		<div className={styles.ChatContainer}>
 			<div className={styles.ChatBox}>
-				<div className={styles.ChatList}></div>
+				<div className={styles.ChatList}>
+					{allMessages.map((item, index) => {
+						return (
+							<SingleChatBox
+								name={item.name}
+								message={item.message}
+								time={item.time}
+							/>
+						);
+					})}
+				</div>
 				<div className={styles.InputBox}>
 					<div>
-						<input type="text" className={styles.inputMessageBox} />
+						<input
+							type="text"
+							className={styles.inputMessageBox}
+							onChange={(e) => setCurrentMessage(e.target.value)}
+							value={currentMessage}
+						/>
 					</div>
-					<div className={styles.sendButton}>
+					<div
+						className={styles.sendButton}
+						onClick={() => {
+							onSendingMessage();
+						}}
+					>
 						<IoSend />
 					</div>
 				</div>
--- apps/web/app/components/SingleChatBox.tsx
+++ apps/web/app/components/SingleChatBox.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import styles from "../page.module.css";
 
-export const ChatContainer: React.FC = () => {
+interface IMessageProps {
+	name: string;
+	message: string;
+	time: string;
+}
+
+export const ChatContainer: React.FC<IMessageProps> = ({
+	name,
+	message,
+	time,
+}) => {
 	return (
 		<div className={styles.SingleChat}>
-			<div className={styles.chatUserName}>Suraj Singh</div>
+			<div className={styles.chatUserName}>{name}</div>
 			<div className={styles.chatMessage}>
-				<div className={styles.chatText}>Hi How are you buddy?</div>
-				<div className={styles.chatTime}>09:15</div>
+				<div className={styles.chatText}>{message}</div>
+				<div className={styles.chatTime}>{time}</div>
 			</div>
 		</div>
 	);
